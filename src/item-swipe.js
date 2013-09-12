ebfee35db194114afff5984052d58b3394fa1ef9
Dane Macaulay <dane.macaulay@gmail.com>
2013-09-12 11:25:36 -0500

    updates;

--- src/item-swipe.js
+++ src/item-swipe.js
@@ -4,94 +4,89 @@
 angular.module('itemSwipe', ['ngTouch'])
   .directive('itemSwipe', ['$swipe', '$document', '$window', '$timeout',
     function($swipe, $document, $window, $timeout) {
-
-    return function(scope, element, attr) {
-
-      var startCoords, pos, startIndex, $undoDiv, threeD;
-      threeD = false;
-
-      element.css({position : 'relative'});
-      element.wrap('<div class="item-swipe-wrapper"></div>');
-
-      function validMove(coords, element){
-        return element.index() === startIndex ? true : false;
-      }
-      function fullSwipe(coords){
-        return coords.x > element.parent().width()*(1/3) ? true : false;
-      }
-      function updateElementPosition(pos){
-        if(threeD){
-          element.css({
-            '-o-transform' : 'translate(' + pos + 'px)',
-            '-moz-transform' : 'translate(' + pos + 'px)',
-            '-ms-transform' : 'translate(' + pos + 'px)',
-            '-khtml-transform' : 'translate(' + pos + 'px)',
-            '-webkit-transform' : 'translate(' + pos + 'px)',
-          });
-        }else{
-          element.css('left', pos);
-        }
-      }
-
-      scope.proceed = false;
-      scope.$watch('proceed', function(val){
-        if(val){
-          $undoDiv.css('opacity', 1);
-          scope.eliminateItem = $timeout(function() {
-            $undoDiv.unbind('click');
+      return {
+        transclude: true,
+        template: '<div class="item-swipe-wrapper" style="position: relative">' +
+                    '<div class="swiper" ng-style="swiperStyle" ng-transclude style="position: relative"></div>' +
+                    '<div class="undo-div" ng-style="undoStyle" ng-click="proceed = false">Undo</div>' +
+                  '</div>',
+        link: {
+          post: function postLink(scope, iElement, iAttrs, controller) {
+            var startCoords, threeD, $swiper;
+            $swiper = angular.element('.swiper', iElement);
             scope.proceed = false;
-            return scope.$eval(attr.onRemove);
-          }, 1220);
-        }else{
-          $undoDiv.css('opacity', 0);
-          $timeout.cancel(scope.eliminateItem);
-          updateElementPosition(0);
-        }
-      });
+            scope.undoStyle = {
+              opacity: 0,
+              width: '100%',
+              position: 'absolute',
+              left: '70%',
+              top: '0',
+            };
 
-      $undoDiv = angular.element('<div></div>')
-        .addClass('undo-div')
-        .css('opacity', 0)
-        .text('undo')
-        .bind('click', function(){
-          scope.proceed = false;
-          scope.$apply();
-        })
-        .appendTo(element.parent());
-
-      $swipe.bind(element, {
-        'start': function(coords) {
-          startCoords = coords;
-          startIndex = element.index();
-          element.removeClass('moving');
-          element.css('opacity', 0.5);
-        },
-        'cancel': function() {
-          element.addClass('moving');
-        },
-        'move': function(coords) {
-          if(validMove(coords, element)){
-            pos = coords.x - element.width()/2;
-            updateElementPosition(pos);
-            if(coords.x < $document.width()*2/3){
-              // cancel if they've swiped back to the left
-              scope.proceed = false;
-              scope.$apply();
+            function fullSwipe(coords){
+              return coords.x - startCoords.x > $swiper.width()*(1/3) ? true : false;
             }
+
+            function cssPrefix(property, value){
+              var vendors = ['', '-o-','-moz-','-ms-','-khtml-','-webkit-'];
+              var styles = {};
+              for (var i = vendors.length - 1; i >= 0; i--) {
+                styles[vendors[i] + property] = value;
+              }
+              return styles;
+            }
+
+            function updateElementPosition(pos){
+              if('threeD' in iAttrs){
+                $swiper.css(cssPrefix('transform', 'translate(' + pos + 'px)'));
+              }else{
+                $swiper.css('left', pos);
+              }
+            }
+
+            scope.$watch('proceed', function(val){
+              if(val){
+                scope.undoStyle.opacity = 1;
+                scope.eliminateItem = $timeout(function() {
+                  scope.proceed = false;
+                  return scope.$eval(iAttrs.onRemove);
+                }, 1220);
+              }else{
+                scope.undoStyle.opacity = 0;
+                $timeout.cancel(scope.eliminateItem);
+                updateElementPosition(0);
+              }
+            });
+
+            $swipe.bind($swiper, {
+              'start': function(coords) {
+                startCoords = coords;
+                scope.swiperStyle = {opacity: 0.5};
+                scope.$apply();
+              },
+              'cancel': function() {
+                scope.moving = 'moving';
+                scope.swiperStyle = cssPrefix('transition', 'all 0.2s ease-in-out');
+                scope.swiperStyle.opacity = 1;
+                scope.$apply();
+              },
+              'move': function(coords) {
+                updateElementPosition(coords.x - startCoords.x);
+              },
+              'end': function(endCoords) {
+                if (fullSwipe(endCoords)) {
+                  scope.proceed = true;
+                  updateElementPosition($document.width());
+                }else {
+                  scope.proceed = false;
+                  updateElementPosition(0);
+                }
+                scope.swiperStyle = cssPrefix('transition', 'all 0.2s ease-in-out');
+                scope.swiperStyle.opacity = 1;
+                scope.$apply();
+              }
+            });
           }
-        },
-        'end': function(endCoords) {
-          if (fullSwipe(endCoords)) {
-            scope.proceed = true;
-            updateElementPosition($document.width());
-          }else {
-            scope.proceed = false;
-            updateElementPosition(0);
-          }
-          element.addClass('moving');
-          element.css('opacity', 1 );
-          scope.$apply();
         }
-      });
-    };
-  }]);
+      };
+    }]);
